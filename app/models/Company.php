5442469b31d6c1becd0ef09129cde7505e7451a7
Harsh Vakharia <harshjv@gmail.com>
2014-03-21 00:37:38 +0530

    Added accessor and mutuators

--- app/models/Company.php
+++ app/models/Company.php
@@ -6,6 +6,9 @@ class Company extends Eloquent {
   public $timestamps = false;
 
   protected $attrib_delta = null;
+  protected $attrib_ts = null;
+  protected $attrib_as = null;
+  protected $attrib_mp = null;
 
   public function players() {
     return $this->belongsToMany('Player')->withPivot('total_shares');
@@ -15,6 +18,21 @@ class Company extends Eloquent {
     if($this->attributes['market_price'] == 0 ) return 0;
     if(is_null($this->attrib_delta)) $this->attrib_delta = round(100*(($this->attributes['market_price'] - $this->attributes['old_market_price'])/ $this->attributes['market_price']), 2);
     return $this->attrib_delta;
+  }
+
+  public function getPrintTotalSharesAttribute() {
+    if(is_null($this->attrib_ts)) $this->attrib_ts = number_format($this->attributes['total_shares']);
+    return $this->attrib_ts;
+  }
+
+  public function getPrintAvailableSharesAttribute() {
+    if(is_null($this->attrib_as)) $this->attrib_as = number_format($this->attributes['available_shares']);
+    return $this->attrib_as;
+  }
+
+  public function getPrintMarketPriceAttribute() {
+    if(is_null($this->attrib_mp)) $this->attrib_mp = number_format($this->attributes['market_price'], 2);
+    return $this->attrib_mp;
   }
 
 }
--- app/models/Player.php
+++ app/models/Player.php
@@ -5,6 +5,7 @@ class Player extends Eloquent {
   protected $table = "players";
   public $timestamps = false;
   protected $attrib_delta = null;
+  protected $attrib_tcih = null;
 
   public function companies() {
     return $this->belongsToMany('Company')->withPivot('total_shares');
@@ -14,6 +15,11 @@ class Player extends Eloquent {
     if($this->attributes['total_cash_in_hand'] == 0 ) return 0;
     if(is_null($this->attrib_delta)) $this->attrib_delta = round(100 * ( $this->attributes['total_cash_in_hand'] - Config::get('dalalstreet.starting_cash') ) / $this->attributes['total_cash_in_hand'], 2);
     return $this->attrib_delta;
+  }
+
+  public function getPrintTotalCashInHandAttribute() {
+    if(is_null($this->attrib_tcih)) $this->attrib_tcih = number_format($this->attributes['total_cash_in_hand']);
+    return $this->attrib_tcih;
   }
 
 }
